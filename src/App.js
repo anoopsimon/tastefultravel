2418e055d8c6920e857ebd42a0796fd0053575c0
Anoop Simon <s451421990@gmail.com>
2021-04-24 20:02:03 +1000

    fixed logi

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import Profile from './components/Profile';
 import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
-  const { isLoading } = useAuth0();
-  if (isLoading) return <div>Loading..</div>
+  const {isLoading,loginWithRedirect  ,isAuthenticated} = useAuth0();
 
   return (
     <div>
-      <LoginButton />
+      <LoginButton/>
       <LogoutButton />
       <Profile />
     </div>
